15f2cbd11c84d9c072f215d14d453dac854d2e6a
micnabo98 <micnabo98@gmail.com>
2020-05-18 22:36:37 +0200

    Update mapy

--- kontakt.php
+++ kontakt.php
@@ -69,31 +69,7 @@ session_start();
 			</p>
 
 
-				<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
-				<script>
-				    var myMap;
-				    var myLatlng = new google.maps.LatLng(50.030342,22.015634);
-				    function initialize() {
-				        var mapOptions = {
-				            zoom: 13,
-				            center: myLatlng,
-				            mapTypeId: google.maps.MapTypeId.ROADMAP  ,
-				            scrollwheel: false
-				        }
-				        myMap = new google.maps.Map(document.getElementById('map'), mapOptions);
-				        var marker = new google.maps.Marker({
-				            position: myLatlng,
-				            map: myMap,
-				            title: 'Name Of Business',
-				            icon: 'http://www.google.com/intl/en_us/mapfiles/ms/micons/red-dot.png'
-				        });
-				    }
-				    google.maps.event.addDomListener(window, 'load', initialize);
-				</script>
-
-				<div id="map" style="width:100%; height: 50%; ">
-
-				</div>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2562.988709836661!2d22.01339121571622!3d50.03030737941961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spl!2spl!4v1589833986638!5m2!1spl!2spl" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 			</div>
 
 
